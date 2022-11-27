833eae93a83ec8ddbb9f69ba2dddc8680d11dbb4
Tu T. Do <tu.do1@tiki.vn>
2022-11-28 03:56:05 +0700

    Update: done quicksort

--- src/pages/sort.py
+++ src/pages/sort.py
@@ -16,7 +16,7 @@ arr = None;
 with lcol:
     n_elem = st.number_input("Number of element", 
             min_value = 10,
-            max_value = 40,
+            max_value = 100,
             step = 1)
 
     algo = st.selectbox("Algorithm",
@@ -24,11 +24,9 @@ with lcol:
 
     if n_elem:
         # Generate random array with n elements
-        arr = np.random.randint(1, n_elem + 1, n_elem);
+        arr = np.random.randint(1, 100, n_elem);
         arr_str = " ".join([str(x) for x in arr]);
-        st.text_area(label="Array to be sorted",
-                value = arr_str,
-                disabled = True)
+        st.markdown(f"Array to be sorted `{arr_str}`");
 
     
 with rcol:
@@ -36,6 +34,14 @@ with rcol:
         if algo == "BUBBLE SORT":
             from sort_v2 import bbsort;
             seq = bbsort(arr);
+            anim = animate_sort_v2(seq);
+        
+            components.html(anim.to_jshtml(), height=600)
+        
+        if algo == "QUICK SORT":
+            from sort_v2 import quick_sort;
+            seq = [];
+            quick_sort(seq, arr, 0, n_elem - 1);
             anim = animate_sort_v2(seq);
         
             components.html(anim.to_jshtml(), height=600)
--- src/sort_v2.py
+++ src/sort_v2.py
@@ -15,3 +15,44 @@ def bbsort(arr:List[int]):
 
     return seq;
 
+
+
+# Function to find the partition position
+def partition(seq, array, low, high):
+    # Choose the rightmost element as pivot
+    pivot = array[high]
+
+    # Pointer for greater element
+    i = low - 1
+
+    # Traverse through all elements
+    # compare each element with pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+            # If element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i = i + 1
+
+            # Swapping element at i with element at j
+            (array[i], array[j]) = (array[j], array[i])
+            seq.append((array.copy(), (i, j)));
+
+    # Swap the pivot element with
+    # e greater element specified by i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    seq.append((array.copy(), (i+1, high)));
+
+    # Return the position from where partition is done
+    return i + 1
+
+def quick_sort(seq, array, low, high):
+    if low < high:
+        # Find pivot element such that
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(seq, array, low, high)
+
+        # Recursive call on the left of pivot
+        quick_sort(seq, array, low, pi - 1)
+        # Recursive call on the right of pivot
+        quick_sort(seq, array, pi + 1, high)
